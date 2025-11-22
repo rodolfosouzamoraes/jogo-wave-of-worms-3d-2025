a74b707b8846500b053819a1186dbe458133fc2d
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-11-22 00:26:35 -0300

    Mochila que muda de cor com base na vida

--- Assets/PlayerDamage.cs
+++ Assets/PlayerDamage.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
     [SerializeField] float life;
     [SerializeField] GameObject backpack;
+    [SerializeField] Renderer backpackRender;
+    [SerializeField] float emissiveIntensity = 8f;
+    private Material materialLife;
 
     private float maxLife;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,12 +14,14 @@ public class PlayerDamage : MonoBehaviour
     {
         maxLife = life;
         backpack.SetActive(false);
+        materialLife = backpackRender.materials[1];
+        materialLife.EnableKeyword("_EMISSION");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateBackpackColor();
     }
 
     public void DecrementLife(float damage)
@@ -24,6 +29,7 @@ public class PlayerDamage : MonoBehaviour
         life -= damage * Time.deltaTime;
         if (life <= 0){
             life = 0;
+            materialLife.color = new Color(0,0,0);
             //Game Over
         }
     }   
@@ -31,5 +37,16 @@ public class PlayerDamage : MonoBehaviour
     public void ActiveBackpack()
     {
         backpack.SetActive(true);
+    }
+
+    private void UpdateBackpackColor()
+    {
+        if (life > 0)
+        {
+            float porcentagem = life / maxLife;
+            Color baseColor = Color.Lerp(Color.red, Color.green, porcentagem);
+            Color emissiveColor = baseColor * emissiveIntensity;
+            materialLife.SetColor("_EmissionColor", emissiveColor);
+        }        
     }
 }
